76e59985d463317f3086c42f36ae71933f74d761
Emilio <040emilio@gmail.com>
2022-05-18 12:30:56 -0300

    first commit

---
+++ PrimeiroProjetoJAVA/src/br/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		Gato gato = new Gato();
+		/*Livro livro = new livro();*/
+
+		System.out.println(gato);
+		/*System.out.println(livro);*/
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String paginas;
+
+}
